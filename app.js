585521d0eb37bc6206ab4b541ca6c3f73656f23a
Ryan Andrade <119017403+ryxnandrade@users.noreply.github.com>
2023-01-20 22:44:09 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
 //mais um console log de teste:
-console.log("o paylo fez esse commit sem voce ver na aula")
+console.log("o paulo fez esse commit sem voce ver na aula")
 console.log("Enviando para o GitHub")
